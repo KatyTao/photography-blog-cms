0ccff738f02afc329124a262b0ac15ca947641b2
Katy Tao <katyt3253@gmail.com>
2022-03-01 00:06:08 +0800

    update photos schema and controller

--- src/api/photo/controllers/photo.js
+++ src/api/photo/controllers/photo.js
@@ -5,5 +5,22 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const parseAttributes = (data, keys) => {
+  const initialData = data;
+  keys.forEach((key) => {
+    const value = initialData[key]?.data
+    if (value) {
+      initialData[key] = { id: value.id || null, ...value.attributes }
+    }
+  })
+  return initialData
+}
+module.exports = createCoreController('api::photo.photo', ({ strapi }) => ({
 
-module.exports = createCoreController('api::photo.photo');
+  async find(ctx) {
+    const res = await super.find(ctx)
+    const { data, meta } = res;
+    const formattedData = data.map((photo) => ({ id: photo.id, ...parseAttributes(photo.attributes, ['photo', 'category']) }))
+    return { data: formattedData, meta };
+  }
+}));
