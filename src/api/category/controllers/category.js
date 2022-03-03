a9595142fb461c3e225ce0800cefd002f44c7b2c
Katy Tao <katyt3253@gmail.com>
2022-03-04 00:26:12 +0800

    update category controller

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::category.category', ({ strapi }) => ({
+  async find(ctx) {
+    const res = await super.find(ctx)
+    const { data, meta } = res;
+    const formattedData = data.map((category) => ({ id: category.id, ...category.attributes }))
+    return { data: formattedData, meta };
+  }
+}));
