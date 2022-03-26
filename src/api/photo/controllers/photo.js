4eba1ce935c69dc86039da06b4c19a097af5c959
Katy Tao <katyt3253@gmail.com>
2022-03-26 15:05:20 +0800

    flatten categories data

--- src/api/photo/controllers/photo.js
+++ src/api/photo/controllers/photo.js
@@ -9,8 +9,10 @@ const parseAttributes = (data, keys) => {
   const initialData = data;
   keys.forEach((key) => {
     const value = initialData[key]?.data
-    if (value) {
+    if (!Array.isArray(value)) {
       initialData[key] = { id: value.id || null, ...value.attributes }
+    } else {
+      initialData[key] = value.map((item) => ({id: item.id || null, ...item.attributes}))
     }
   })
   return initialData
@@ -20,7 +22,7 @@ module.exports = createCoreController('api::photo.photo', ({ strapi }) => ({
   async find(ctx) {
     const res = await super.find(ctx)
     const { data, meta } = res;
-    const formattedData = data.map((photo) => ({ id: photo.id, ...parseAttributes(photo.attributes, ['photo', 'category']) }))
+    const formattedData = data.map((photo) => ({ id: photo.id, ...parseAttributes(photo.attributes, ['photo', 'categories']) }))
     return { data: formattedData, meta };
   }
 }));
